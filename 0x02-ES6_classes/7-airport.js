e80d3780b90f6aecc3a43f0687fb61548c224da8
erdey <erdeysyoum@gmail.com>
2024-06-28 06:41:53 -0700

    hoisting class

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,32 +1,48 @@
-export default class Airport {
-  constructor(name, code) {
-    this.name = name;
-    this.code = code;
+export class HolbertonClass {
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
   }
 
-  set name(name) {
-    if (typeof name !== 'string') {
-      throw new TypeError('name must be string');
-    }
-    this._name = name;
+  get year() {
+    return this._year;
   }
 
-  get name() {
-    return this._name;
-  }
-
-  set code(code) {
-    if (typeof code !== 'string') {
-      throw new TypeError('code must be string');
-    }
-    this._code = code;
-  }
-
-  get code() {
-    return this._code;
-  }
-
-  get [Symbol.toStringTag]() {
-    return this._code;
+  get location() {
+    return this._location;
   }
 }
+
+export class StudentHolberton {
+  constructor(firstName, lastName, holbertonClass) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
+  }
+
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
+  }
+
+  get holbertonClass() {
+    return this._holbertonClass;
+  }
+
+  get fullStudentDescription() {
+    return `${this._firstName} ${this._lastName} -
+${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+  }
+}
+
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
+const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = new StudentHolberton('John', 'Doe', class2020);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+
+const listOfStudents = [student1, student2, student3, student4, student5];
+
+export default listOfStudents;
