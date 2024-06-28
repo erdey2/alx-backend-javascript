25487ab0049572a0381c69bb25534cc9b4a29f02
erdey <erdeysyoum@gmail.com>
2024-06-28 03:23:42 -0700

    abstract class Building Added

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,8 +1,16 @@
 export default class Building {
   constructor(sqft) {
     this.sqft = sqft; // use the setter to initialize sqft
-  }
 
+  if (this.constructor !== Building) {
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error(
+       'Class extending Building must override evacuationWarningMessage',
+      );
+    }
+  }
+ }
+  
   set sqft(sqft) {
     if (typeof sqft !== 'number') {
       throw new TypeError('sqft must be number');
