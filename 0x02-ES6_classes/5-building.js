91db11ab80ac63d7ccbca1ffbef7794c12a3f6f5
erdey <erdeysyoum@gmail.com>
2024-06-28 01:33:49 -0700

    abstract class Building Added

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,20 +1,24 @@
 export default class Building {
   constructor(sqft) {
-    this.sqft = sqft;
+    this.sqft = sqft; // use the setter to initialize sqft
   }
 
   set sqft(sqft) {
     if (typeof sqft !== 'number') {
-      throw new Error('sqft must be number');
+      throw new TypeError('sqft must be number');
     }
     this._sqft = sqft;
   }
 
   get sqft() {
-    return thsi._sqft;
+    return this._sqft;
   }
-  
+
+  // This method must be overridden by subclasses
+  // Disable ESLint rule for this method
+  /* eslint-disable class-methods-use-this */
   evacuationWarningMessage() {
-    throw new Error("Class extending Building must override evacuationWarningMessage");
+    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
+  /* eslint-enable class-methods-use-this */
 }
