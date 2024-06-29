6956721338b5cd2fadc2e23905906f18cbe867c4
erdey <erdeysyoum@gmail.com>
2024-06-29 05:16:49 -0700

    create report object

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,8 +1,8 @@
 export default function createReportObject(employeeList) {
   return {
     allEmployees: employeeList,
-    getNumberOfDepartments(list) { 
+    getNumberOfDepartments(list) {
       return Object.keys(list).length;
-    }
+    },
   };
 }
