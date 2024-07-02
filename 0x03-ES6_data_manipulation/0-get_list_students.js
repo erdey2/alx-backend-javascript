db29b6cabbe1fd24b8e7807d816fb06b78d32684
erdey <erdeysyoum@gmail.com>
2024-07-01 23:25:43 -0700

    create list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,8 @@
 export default function getListStudents() {
   const datas = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstNmae: 'Serena', location: 'San Francisco' },
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco', },
+    { id: 2, firstName: 'James', location: 'Columbia', },
+    { id: 5, firstNmae: 'Serena', location: 'San Francisco', },
   ];
   return datas;
 }
