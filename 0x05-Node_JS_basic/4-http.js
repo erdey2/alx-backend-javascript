f3801f5024d7e0e1256cc5d9427d485cb40f50d2
erdey <erdeysyoum@gmail.com>
2024-08-22 08:43:48 +0000

    create a server on specific port

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,5 @@
-'use restrict'
 const http = require('http');
-app = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
 	res.writeHead(200, {
 		'Content-Type': 'text/plain'
 	});
