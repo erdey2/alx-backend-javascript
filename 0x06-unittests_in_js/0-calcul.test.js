391f0882d88eb47a5e435d8c9e4f5af633b2f7b8
erdey <erdeysyoum@gmail.com>
2024-08-22 13:07:05 +0000

    testing updated

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,18 +1,20 @@
-'use_strict'
-const calculateNumber = require('./0-calcul')
-const assert = require('assert');
-describe('calculateNumber', function() {
-	it('Whole positive numbers', function() {
-		assert(calculateNumber(1, 3) == 4);
-	});
-	it('one whole and one decimal ', function() {
-                assert(calculateNumber(1, 3.7) == 4);
-        });
-	it('two decimal sum less than 5', function() {
-                assert(calculateNumber(1.2, 3.7) == 4);
-        });
-	it('two decimal sum greater than 5', function() {
-                assert(calculateNumber(1.5, 3.7) == 4);
-        });
-});
+const calculateNumber = require('./0-calcul');
 
+const assert = require('assert');
+
+describe('calculateNumber', function() {
+  it('Whole positive numbers', function() {
+    assert(calculateNumber(1, 3) == 4);
+  });
+
+  it('one whole and one decimal ', function() {
+    assert(calculateNumber(1, 3.7) == 4);
+  });
+  
+  it('two decimal sum less than 5', function() {
+    assert(calculateNumber(1.2, 3.7) == 4);
+  });
+  it('two decimal sum greater than 5', function() {
+    assert(calculateNumber(1.5, 3.7) == 4);
+  });
+});
