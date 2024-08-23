94c7b318f5b86dd02b2ca1cb4797d41c1d5902d0
erdey <erdeysyoum@gmail.com>
2024-08-23 09:28:54 +0000

    simple test v2

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,22 +1,23 @@
 const calculateNumber = (type, a, b) => {
-	if (type === 'SUM')
-	{
-		return Math.round(a) + Math.round(b);
-	}
-	else if (type === 'SUBTRACT')
-	{
-		return Math.round(a) - Math.round(b);
-	}
-	else if (type === 'DIVIDE')
-	{
-		if (Math.round(b) === 0)
-		{
-			throw new TypeError('Error');
-		}
-		else
-		{
-			return Math.round(a) / Math.round(b);
-		}
-	}
+  if (type === 'SUM')
+  {
+    return Math.round(a) + Math.round(b);
+  }
+  else if (type === 'SUBTRACT')
+  {
+    return Math.round(a) - Math.round(b);
+  }
+  else if (type === 'DIVIDE')
+  {
+    if (Math.round(b) === 0)
+    {
+      throw new TypeError('Error');
+    }
+    else
+    {
+      return Math.round(a) / Math.round(b);
+    }
+  }
+  return 0;
 }
 module.exports = calculateNumber;
