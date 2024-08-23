beca4af029e01da703b8cd4660be231dc7a6840a
erdey <erdeysyoum@gmail.com>
2024-08-23 09:13:17 +0000

    simple test v2

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,16 @@
+const calculateNumber = require('./1-calcul')
+const assert = require('assert');
+describe('calculateNumber', function() {
+	it('check sum', function() {
+		assert(calculateNumber('SUM', 1.4, 4.5) == 6);
+	});
+	it('check subtraction ', function() {
+                assert(calculateNumber('SUBTRACT', 1.4, 4.5) == -4);
+        });
+	it('check division', function() {
+                assert(calculateNumber('DIVIDE', 1.4, 4.5) == 0.2);
+        });
+	it('check division', function() {
+                assert(calculateNumber('DIVIDE', 1.4, 0) == 'Error');
+        });
+});
